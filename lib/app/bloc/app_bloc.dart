00748cb7cb0868026f92032f032ef0e61de0534b
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-10-19 00:10:05 +0200

    49. Firebase integration
    1. App Bloc
    - state
    - event

---
+++ lib/app/bloc/app_bloc.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+part 'app_event.dart';
+part 'app_state.dart';
