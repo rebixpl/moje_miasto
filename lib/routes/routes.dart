4cca6914623a52ade6d5ee9016f6d8f6bfaee961
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-10-19 09:50:27 +0200

    50. Firebase integration
    1. App Bloc
    - bloc
    - routes

---
+++ lib/routes/routes.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/widgets.dart';
+// import 'package:moje_miasto/app/app.dart';
+
+// List<Page<dynamic>> onGenerateAppViewPages(
+//   AppStatus state,
+//   List<Page<dynamic>> pages,
+// ) {
+//   switch (state) {
+//     case AppStatus.authenticated:
+//       // return [HomePage.page()];
+      
+//     case AppStatus.unauthenticated:
+//       // return [LoginPage.page()];
+//   }
+// }
