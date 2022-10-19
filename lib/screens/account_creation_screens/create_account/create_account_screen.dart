ac4f37a41fef8a07bf4388e4b627c5ec12177adb
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-10-19 12:51:41 +0200

    54. Firebase integration
    1. Sign Up Page
    - sign up form
    - sign up cubit
    - sign up state

--- lib/screens/account_creation_screens/create_account/create_account_screen.dart
+++ lib/screens/account_creation_screens/create_account/create_account_screen.dart
@@ -1,6 +1,9 @@
+import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
 import 'package:moje_miasto/screens/account_creation_screens/ca_more_info/ca_more_info_screen.dart';
+import 'package:moje_miasto/screens/account_creation_screens/create_account/cubit/sign_up_cubit.dart';
 import 'package:moje_miasto/screens/account_creation_screens/create_account/widgets/avatar_selector/avatar_selector.dart';
 import 'package:moje_miasto/screens/account_creation_screens/create_account/widgets/avatar_selector/cubits/avatar_selector_cubit.dart';
 import 'package:moje_miasto/screens/account_creation_screens/create_account/widgets/create_account_form.dart';
@@ -26,6 +29,9 @@ class CreateAccountScreen extends StatelessWidget {
         BlocProvider(
           create: (context) => AvatarSelectorCubit(),
         ),
+        BlocProvider(
+          create: (_) => SignUpCubit(context.read<AuthenticationRepository>()),
+        ),
       ],
       child: Scaffold(
         body: SafeArea(
@@ -33,52 +39,51 @@ class CreateAccountScreen extends StatelessWidget {
             padding: const EdgeInsets.all(AppTheme.kDefaultPadding),
             child: Column(
               children: [
-                Expanded(
-                  child: SingleChildScrollView(
-                    physics: const BouncingScrollPhysics(),
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 20.0),
-                        createAccountText(context),
-                        const SizedBox(height: 20.0),
-                        const FullWidthDivider(),
-                        const SizedBox(height: 20.0),
-                        tellUsMoreText(context),
-                        const SizedBox(height: 20.0),
-                        CreateAccountForm(
-                          formKey: _formKey,
-                          emailController: _emailController,
-                          passwordController: _passwordController,
-                          usernameController: _usernameController,
-                        ),
-                        const SizedBox(height: 20.0),
-                        const SelectAvatar(),
-                        const SizedBox(height: 40.0),
-                        BigElevatedButton(
-                          text: 'Kontynuuj',
-                          onTap: () {
-                            // TODO: also validate avatar selector, or create a cubit with default first avatar
-                            if (_formKey.currentState!.validate()) {
-                              // TODO: navigate to CA Additional Informations Screen
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => CAMoreInfoScreen(),
-                                ),
-                              );
-                              debugPrint(
-                                  'validation successfull, navigate to CA Additional Informations Screen');
-                            } else {
-                              debugPrint('ERROR validating create account!');
-                            }
-                          },
-                        ),
-                        const SizedBox(height: 30.0),
-                        const GoogleAuthBtnCentred(),
-                        const SizedBox(height: 20.0),
-                        bottomTosTexts(context),
-                        const SizedBox(height: 20.0),
-                      ],
+                BlocListener<SignUpCubit, SignUpState>(
+                  listener: (context, state) {
+                    if (state.status.isSubmissionSuccess) {
+                      Navigator.of(context).pop();
+                    } else if (state.status.isSubmissionFailure) {
+                      ScaffoldMessenger.of(context)
+                        ..hideCurrentSnackBar()
+                        ..showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              state.errorMessage ?? 'Sign Up Failure',
+                            ),
+                          ),
+                        );
+                    }
+                  },
+                  child: Expanded(
+                    child: SingleChildScrollView(
+                      physics: const BouncingScrollPhysics(),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 20.0),
+                          createAccountText(context),
+                          const SizedBox(height: 20.0),
+                          const FullWidthDivider(),
+                          const SizedBox(height: 20.0),
+                          tellUsMoreText(context),
+                          const SizedBox(height: 20.0),
+                          CreateAccountForm(
+                            formKey: _formKey,
+                            emailController: _emailController,
+                            passwordController: _passwordController,
+                            usernameController: _usernameController,
+                          ),
+                          const SizedBox(height: 20.0),
+                          const SelectAvatar(),
+                          const SizedBox(height: 40.0),
+                          _SignUpButton(formKey: _formKey),
+                          const SizedBox(height: 30.0),
+                          const GoogleAuthBtnCentred(),
+                          const SizedBox(height: 20.0),
+                          bottomTosTexts(context),
+                          const SizedBox(height: 20.0),
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -87,6 +92,52 @@ class CreateAccountScreen extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class _SignUpButton extends StatelessWidget {
+  const _SignUpButton({
+    Key? key,
+    required GlobalKey<FormState> formKey,
+  })  : _formKey = formKey,
+        super(key: key);
+
+  final GlobalKey<FormState> _formKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SignUpCubit, SignUpState>(
+      buildWhen: (previous, current) => previous.status != current.status,
+      builder: (context, state) {
+        return state.status.isSubmissionInProgress
+            ? const CircularProgressIndicator()
+            : BigElevatedButton(
+                key: const Key('signUpForm_continue_raisedButton'),
+                text: 'Kontynuuj',
+                onTap: () {
+                  if (state.status.isValidated) {
+                    context.read<SignUpCubit>().signUpFormSubmitted();
+                  }
+                },
+                // onTap: () {
+                //   // TODO: also validate avatar selector, or create a cubit with default first avatar
+                //   if (_formKey.currentState!.validate()) {
+                //     // TODO: navigate to CA Additional Informations Screen
+                //     Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //         builder: (context) => CAMoreInfoScreen(),
+                //       ),
+                //     );
+                //     debugPrint(
+                //         'validation successfull, navigate to CA Additional Informations Screen');
+                //   } else {
+                //     debugPrint('ERROR validating create account!');
+                //   }
+                // },
+              );
+      },
     );
   }
 }
--- lib/screens/account_creation_screens/create_account/widgets/create_account_form.dart
+++ lib/screens/account_creation_screens/create_account/widgets/create_account_form.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:moje_miasto/screens/account_creation_screens/create_account/cubit/sign_up_cubit.dart';
 import 'package:moje_miasto/shared/app/text_fields/my_text_field.dart';
 import 'package:moje_miasto/shared/app/text_fields/password_text_field.dart';
 import 'package:moje_miasto/shared/app/text_fields/text_field_validators.dart';
@@ -25,17 +27,9 @@ class CreateAccountForm extends StatelessWidget {
       child: Column(
         children: [
           // Email Form
-          MyTextField(
-            myController: emailController,
-            fieldName: 'Email',
-            myIcon: FontAwesomeIcons.solidEnvelope,
-            validator: Validators.validateEmail,
-          ),
+          _EmailInput(emailController: emailController),
           // Password Form
-          PasswordTextField(
-            passwordController: passwordController,
-            validator: Validators.validatePasswordCreateAccount,
-          ),
+          _PasswordInput(passwordController: passwordController),
           // Username Form
           MyTextField(
             myController: usernameController,
@@ -45,6 +39,57 @@ class CreateAccountForm extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class _PasswordInput extends StatelessWidget {
+  const _PasswordInput({
+    Key? key,
+    required this.passwordController,
+  }) : super(key: key);
+
+  final TextEditingController passwordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SignUpCubit, SignUpState>(
+      buildWhen: (previous, current) => previous.password != current.password,
+      builder: (context, state) {
+        return PasswordTextField(
+          key: const Key('signUpForm_passwordInput_textField'),
+          onChanged: (password) =>
+              context.read<SignUpCubit>().passwordChanged(password),
+          passwordController: passwordController,
+          validator: Validators.validatePasswordCreateAccount,
+        );
+      },
+    );
+  }
+}
+
+class _EmailInput extends StatelessWidget {
+  const _EmailInput({
+    Key? key,
+    required this.emailController,
+  }) : super(key: key);
+
+  final TextEditingController emailController;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SignUpCubit, SignUpState>(
+      buildWhen: (previous, current) => previous.email != current.email,
+      builder: (context, state) {
+        return MyTextField(
+          key: const Key('signUpForm_emailInput_textField'),
+          myController: emailController,
+          onChanged: (email) => context.read<SignUpCubit>().emailChanged(email),
+          fieldName: 'Email',
+          myIcon: FontAwesomeIcons.solidEnvelope,
+          validator: Validators.validateEmail,
+        );
+      },
     );
   }
 }
